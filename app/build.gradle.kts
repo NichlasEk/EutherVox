19a98034257a7018c291e8e19fb4baa97eb03531
Nichlas <nichlas.eklof@gmail.com>
2026-08-24 20:11:37 +0200

    Bump robot dashboard APK to beta 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "se.euther.euthervox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 34
-        versionName = "0.19.0-beta.10"
+        versionCode = 35
+        versionName = "0.19.0-beta.11"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
